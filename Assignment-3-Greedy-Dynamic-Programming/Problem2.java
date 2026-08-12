7bcb5b3b08a082437adf11f30602bfdbe0fc7ebc
vanshikatech <avanshika70@gmail.com>
2026-08-12 22:53:39 +0530

    Create Problem2.java

---
+++ Assignment-3-Greedy-Dynamic-Programming/Problem2.java
@@ -0,0 +1,76 @@
+import java.io.*;
+
+public class MaximumLearningPoints {
+
+    private static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) {
+                    return -1;
+                }
+            }
+            return buffer[ptr++];
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') {
+                if (c == -1) {
+                    return -1;
+                }
+            }
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            long value = 0;
+            while (c > ' ') {
+                value = value * 10 + (c - '0');
+                c = read();
+            }
+
+            return value * sign;
+        }
+
+        int nextInt() throws IOException {
+            return (int) nextLong();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+
+        int n = fs.nextInt();
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long prevTwo = 0;
+        long prevOne = 0;
+
+        for (int i = 0; i < n; i++) {
+            long points = fs.nextLong();
+
+            long take = prevTwo + points;
+            long skip = prevOne;
+
+            long current = Math.max(take, skip);
+
+            prevTwo = prevOne;
+            prevOne = current;
+        }
+
+        System.out.println(prevOne);
+    }
+}
